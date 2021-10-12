c85f554b253041ce9e385826f72586517d3aeb18
rohanisitohang <rohanisitohang15@gmail.com>
2021-10-12 17:45:07 +0700

    rohanijava

---
+++ RohaniJava.java
@@ -0,0 +1,31 @@
+public class RohaniJava{
+  public static void main(String[] args) {
+  	// Membuat nama alamat dari peserta
+    // first name , lastname, age, phone number, address, and other
+    //System.out.println("Hello World");
+    
+    // Inisialisasi
+    String first_name = "";
+    String last_name = "";
+    String phone_number;
+    String address = "";
+    int age = 0;
+    
+    // Input variabel
+    first_name = "Rohani Susanti";
+    last_name = "Sitohang";
+    phone_number = "081275939725";
+    address = "BATAM";
+    age = 21;
+    
+    
+    // Print in to screen
+    System.out.println("=======================================");
+    System.out.println("||      First name   = " + first_name );
+    System.out.println("||      Last name    = " + last_name);
+    System.out.println("||      Phone number = " + phone_number);
+    System.out.println("||      Address      = " + address);
+    System.out.println("||      Age          = " + age);
+    System.out.println("=======================================");
+  }
+}
